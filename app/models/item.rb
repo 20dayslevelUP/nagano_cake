abbf0c091d501d55805bcdfd4774bdcb8ccb80ff
EC2 Default User <ec2-user@ip-172-31-92-58.ec2.internal>
2022-03-18 12:55:14 +0000

    [Add] モデル内追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,20 @@
 class Item < ApplicationRecord
+
+  has_many :cart_items
+  has_many :order_details
+
+  belongs_to :ganre
+
+  has_one_attached :image
+
+  enum is_active: { 販売中: true, 売切れ: false }
+
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
 end
