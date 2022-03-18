315cc27efdc6d27bdd1cb1afc4f96a366f60a688
EC2 Default User <ec2-user@ip-172-31-92-249.ec2.internal>
2022-03-18 06:13:55 +0000

    [add]カート内商品モデルのバリデーション

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,5 +3,5 @@ class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
   
-  validates :quantity, presence: true, length: {maximum: 20, minimum: 0}, numericality: true #
+  validates :quantity, presence: true, length: {maximum: 20, minimum: 0}, numericality: true #２０個制限
 end
