d299f1673ce735ebbc5d728233ba492d6ff3daac
EC2 Default User <ec2-user@ip-172-31-92-249.ec2.internal>
2022-03-18 06:10:41 +0000

    [add]カート内商品モデル

---
+++ app/models/cart_item.rb
@@ -0,0 +1,7 @@
+class CartItem < ApplicationRecord
+  
+  belongs_to :customer
+  belongs_to :item
+  
+  validates :quantity, presence: true, length: {maximum: 20, minimum: 0}, numericality: true #
+end
