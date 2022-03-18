843197d1087b472218e322b08444b21fe3e09edf
EC2 Default User <ec2-user@ip-172-31-92-58.ec2.internal>
2022-03-18 17:41:00 +0000

    [Add] admin/items index,show,edit追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -8,22 +8,31 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to admin_item(@item)
+      redirect_to admin_item_path(@item)
     else
       render 'new'
     end
   end
 
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item)
+    else
+      render :edit
+    end
   end
 
   private
