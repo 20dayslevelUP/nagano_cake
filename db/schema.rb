502c9fc9469e5b91e0453a418df340757e66f0db
Sakie Kawamichi <sakie0313@gmail.com>
2022-03-19 05:35:29 +0000

    []

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_18_052249) do
+ActiveRecord::Schema.define(version: 2022_03_19_011737) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -39,7 +39,6 @@ ActiveRecord::Schema.define(version: 2022_03_18_052249) do
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
-
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
