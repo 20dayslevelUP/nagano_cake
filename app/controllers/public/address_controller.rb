68bd5b91f9420c82180c171b6da032c51c50029e
EC2 Default User <ec2-user@ip-172-31-5-181.ec2.internal>
2022-03-20 00:57:48 +0000

    [Add]customerkコメントアウト、addressesのcontrollerとview

--- app/controllers/public/address_controller.rb
+++ app/controllers/public/address_controller.rb
@@ -1,17 +1,44 @@
 class Public::AddressController < ApplicationController
+  before_action :authenticate_customer!
+  before_action :set_address, only: [:edit, :update, :destroy]
 
   def index
+    @addresses = current_customer.addresses.all
+    @address = Address.new
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to addresses_path
+    else
+      @address = Address.all #bookers2の記述パクった、よく分かってない
+      render 'index'
+    end
   end
 
   def edit
   end
 
   def update
+    @address.update(address_params)
+    redirect_to addresses_path
   end
 
   def destroy
+    @address.destroy
+    redirect_to addresses_path
   end
+
+   private
+
+  def set_address
+    @address = Address.find(params[:id])
+  end
+
+  def address_params
+    params.require(:address).permit(:post_code, :address, :name, :customer_id)
+  end
+
 end
