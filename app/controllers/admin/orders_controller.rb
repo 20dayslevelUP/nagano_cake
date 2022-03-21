3212a25e9933fd39b89fbc1e92379da14ed28a40
Sakie Kawamichi <sakie0313@gmail.com>
2022-03-21 11:00:57 +0000

    [Add]管理者側トップページ(注文履歴)追加

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,12 @@
 class Admin::OrdersController < ApplicationController
 
   def index
-    @orders = Order.All
+    @orders = Order.all
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_ditails = @order.order_ditails
   end
 
   def update
