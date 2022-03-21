2e4cd6a51334ca9037d903b664efd94400fd7588
Sakie Kawamichi <sakie0313@gmail.com>
2022-03-21 05:03:57 +0000

    [fix]header:cadminをadminに修正

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -6,5 +6,6 @@ class OrderDetail < ApplicationRecord
   validates :price, numericality: true
   validates :production_status, presence: true
 
+  #enum status: { waiting_payment: 0, confirm_payment: 1, production: 2, send_ready: 3, send_complete: 4 }
   enum production_status: { 着手不可: 0, 製作待ち: 1, 製作中: 2, 製作完了: 3 } #←""つける？
 end
