66dcdbc06cd113d568622c4538939d0a7e58bba6
EC2 Default User <ec2-user@ip-172-31-92-249.ec2.internal>
2022-03-22 01:05:09 +0000

    [add]カート機能（cart_item)

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,12 +1,12 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items = CartItem.all
-    @total_price = 0
+    @cart_items = current_customer.cart_items.all
+    @total_price =  @cart_items.inject(0) { |sum, item| sum + item.subtotal }
   end
 
   def create
       @cart_item = CartItem.new(cart_item_params)
-      @cart_item.costomer_id = current_customer.id
+      @cart_item.customer_id = current_customer.id
     # 追加した商品がカート内に存在するかの判別
     if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
       # カート内の個数をフォームから送られた個数分追加する
@@ -22,7 +22,7 @@ class Public::CartItemsController < ApplicationController
 
   def update
     @cart_item = CartItem.find(params[:id])
-    @cart_item.update(cart_item_params[:item_id])
+    @cart_item.update(quantity: params[:cart_item][:quantity])
     redirect_to cart_items_path
   end
 
