d4857429313eef6cd956101fd0f7800c81535b7a
EC2 Default User <ec2-user@ip-172-31-92-249.ec2.internal>
2022-03-23 01:05:31 +0000

    [add]管理者ログインまわり、ヘッダーのカートアイコン

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  # 顧客はログインしないとaboutページ以外遷移できない設定
+  # before_action :authenticate_customer!, except: [:about]
+  # 管理者はログインしないとaboutページ以外遷移できない設定
+  # before_action :authenticate_admin!, except: [:about]
 end
