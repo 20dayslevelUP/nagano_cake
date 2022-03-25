dc583d42626222ece0fc11d1dc5b7af8d4b174a2
EC2 Default User <ec2-user@ip-172-31-92-249.ec2.internal>
2022-03-25 01:03:52 +0000

    [add]カスタマーログイン

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -5,7 +5,7 @@ class Admin::OrderDetailsController < ApplicationController
     @order_detail.update(order_detail_params)
     @order = Order.find_by(id: @order_detail.order_id)
     @order_details = @order.order_details.all
-      if @order_details.where(production_status: 2).any?#もしも一つでも存在したら？
+      if @order_details.where(production_status: 2).any? #もしも一つでも存在したら？
       @order.update(status: 2)
       redirect_to admin_order_path(@order)
     #binding.pry
